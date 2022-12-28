678ee4a4ac40d3ad967b42d69558990dd64db0dc
Dmitriy Sarafanov <d.sarafanoff@gmail.com>
2022-12-28 18:39:58 +0100

    WIP. Added SwipeLayout lib, main fragment resource.

---
+++ app/src/main/java/com/ds/money_manager/feature/main/fragment/MainFragment.kt
@@ -0,0 +1,7 @@
+package com.ds.money_manager.feature.main.fragment
+
+import com.ds.money_manager.base.presentation.fragment.BaseFragment
+import com.ds.money_manager.base.presentation.fragment.DialogsSupportFragment
+
+class MainFragment : BaseFragment {
+}
