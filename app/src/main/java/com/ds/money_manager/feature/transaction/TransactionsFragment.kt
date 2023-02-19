8f65558df0915969858877db522249eca45dc97a
Dmitriy Sarafanoff <d.sarafanoff@gmail.com>
2023-02-19 02:21:54 +0100

    WIP. Partly implemented transactions fragment.

---
+++ app/src/main/java/com/ds/money_manager/feature/transaction/TransactionsFragment.kt
@@ -0,0 +1,11 @@
+package com.ds.money_manager.feature.transaction
+
+import com.ds.money_manager.base.presentation.fragments.DialogsSupportFragment
+import com.ds.money_manager.databinding.FragmentTransactionsBinding
+
+class TransactionsFragment :
+    DialogsSupportFragment<FragmentTransactionsBinding, TransactionsViewModel>() {
+    override fun initViews() {
+
+    }
+}
